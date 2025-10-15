87c1e0667cdff910637fbaf7bdbdcdfc37282f42
Cinnamon Roll <139164290+CinnamonRoll09@users.noreply.github.com>
2025-10-15 22:19:52 +0530

    Simplify access for 'cinnamon roll' user
    Removed password check for 'cinnamon roll' user.

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 var name = prompt("Name?");
 
 if (name.toLowerCase() == "cinnamon roll") {
-    var psswd = prompt("Password!");
-    if (psswd == "1234") {
-
-    } else {
-        window.location.href = "404.html";
-    }
+    
 } else {
     window.location.href = "404.html";
 }
