28c4b3b63c3e2677bfeca743a2360e3ce01c049e
Cinnamon Roll <139164290+CinnamonRoll09@users.noreply.github.com>
2025-10-17 14:50:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-function button() {
+function login() {
+    const username = document.querySelector(".username").value;
+    const password = document.querySelector(".password").value;
 
-    const username = document.querySelector(".username").value
-    const password = document.querySelector(".password").value
-
-    if (username.toLowerCase() == "cinnamon roll" && password == "2704") {
-        document.querySelector(".welcome").innerHTML = "Welcome"
+    if (username.toLowerCase() === "cinnamon roll" && password === "2704") {
+        document.querySelector(".welcome").innerHTML = "Welcome";
         document.querySelector('.pwho').innerHTML = `<a class="pwho" href="../Cinnamon/cinnamon.html">&nbsp;Cinnamon!</a>`;
     } else {
         window.location.href = "../index.html";
     }
 }
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        login();
+    }
+});
